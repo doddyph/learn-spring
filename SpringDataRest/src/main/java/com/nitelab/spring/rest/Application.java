e590395ed3a34ccb732ef2ea217d9659a468350d
doddyph <d0dd33ph@gmail.com>
2016-10-08 10:46:38 +0700

    Add Sprind Data Rest sample project

---
+++ SpringDataRest/src/main/java/com/nitelab/spring/rest/Application.java
@@ -0,0 +1,19 @@
+package com.nitelab.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Created by dph on 08/10/16.
+ */
+@SpringBootApplication
+@EntityScan(basePackages = "com.nitelab.spring.rest.model")
+@EnableJpaRepositories(basePackages = "com.nitelab.spring.rest.repository")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ SpringDataRest/src/main/java/com/nitelab/spring/rest/repository/PersonRepository.java
@@ -0,0 +1,17 @@
+package com.nitelab.spring.rest.repository;
+
+import com.nitelab.spring.rest.model.Person;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * Created by dph on 08/10/16.
+ */
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+    List<Person> findByLastName(@Param("name") String name);
+}
